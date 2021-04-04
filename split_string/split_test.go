2f0e048c57c0f13d3f6e3391cfd517f9214d422b
Administrator <972051603@qq.com>
2021-04-04 22:58:58 +0800

    feat:增加了测试问题

--- split_string/split_test.go
+++ split_string/split_test.go
@@ -15,3 +15,15 @@ func TestSplit(t *testing.T) {
 
 	}
 }
+
+func TestSplit_2(t *testing.T) {
+
+	got := Split("abvcddbbx", "b")
+	want := []string{"a", "vcdd", "", "x"}
+
+	if !reflect.DeepEqual(got, want) {
+
+		t.Errorf("want: %v,but got: %v", want, got)
+	}
+
+}
